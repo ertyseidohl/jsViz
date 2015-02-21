d6088f7e485cb6422f3ac3eda8335764e34db255
Emil Ng <github@alphabeticaldisorder.com>
2015-02-21 13:45:26 -0500

    add read write editor split

--- src/jsvis.js
+++ src/jsvis.js
@@ -1,7 +1,12 @@
-var editor = ace.edit("editor");
-editor.setTheme("ace/theme/monokai");
-editor.getSession().setMode("ace/mode/javascript");
-var aceDoc = editor.getSession().getDocument();
+var readEditor = ace.edit("read-editor");
+readEditor.setTheme("ace/theme/monokai");
+readEditor.getSession().setMode("ace/mode/javascript");
+readEditor.setReadOnly(true);
+
+var writeEditor = ace.edit("write-editor");
+writeEditor.setTheme("ace/theme/monokai");
+writeEditor.getSession().setMode("ace/mode/javascript");
+var aceDoc = writeEditor.getSession().getDocument();
 var timeOutHandle;
 
 var timeOutCallback = function(code) {
